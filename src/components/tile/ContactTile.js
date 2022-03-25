9359b8daf9bb2a3ca2ce2bfa504514601d128cdc
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-25 03:49:06 -0400

    Needs to fix contact table

--- src/components/tile/ContactTile.js
+++ src/components/tile/ContactTile.js
@@ -3,9 +3,24 @@ import React from "react";
 export const ContactTile = ({ tile }) => {
     return (
         <div className="tile-container">
-            <p>{tile.name}</p>
-            <p>{tile.email}</p>
-            <p>{tile.phone}</p>
+            <table style={{borderStyle: "dashed", width: "100%"}}>
+                <thead>
+                <tr style={{borderBottom: "5px"}}>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td>{tile.name}</td>
+                    <td>{tile.phone}</td>
+                    <td>{tile.email}</td>
+                </tr>
+                </tbody>
+
+            </table>
         </div>
     );
 };
