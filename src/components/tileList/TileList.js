77ffc8ed167f682cad4732fd58df2dd82807f9f0
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-25 02:36:32 -0400

    Needs to fix tilelist

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {Tile} from "../tile/Tile";
 
-export const TileList = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+export const TileList = (props) => {
+
+    let tiles = props.tiles;
+
+    return (
+        <div>
+            {tiles.map((tile, index) => {
+                <Tile key={index} tile={tile} />
+            })}
+        </div>
+    );
 };
