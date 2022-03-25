5eddf25fe1ed163e8d9402e8c4ffbddb0206e0f6
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-25 02:09:33 -0400

    Add to app.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,63 @@
-import React from "react";
-import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+import React, {useState} from "react";
+import {Switch, Route, Redirect, NavLink} from "react-router-dom";
 
-import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
-import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+import {AppointmentsPage} from "./containers/appointmentsPage/AppointmentsPage";
+import {ContactsPage} from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+    /*
+    Define state variables for
+    contacts and appointments
+    */
+    const [contacts, setContacts] = useState([]);
+    const [appointments, setAppointments] = useState([]);
 
-  const ROUTES = {
-    CONTACTS: "/contacts",
-    APPOINTMENTS: "/appointments",
-  };
+    const
+        ROUTES = {
+            CONTACTS: "/contacts",
+            APPOINTMENTS: "/appointments",
+        };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+    /*
+    Implement functions to add data to
+    contacts and appointments
+    */
+    const setNewContacts = (newContacts) => {
+        setContacts([...contacts, newContacts]);
+    }
 
-  return (
-    <>
-      <nav>
-        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
-          Contacts
-        </NavLink>
-        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
-          Appointments
-        </NavLink>
-      </nav>
-      <main>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to={ROUTES.CONTACTS} />
-          </Route>
-          <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
-          </Route>
-          <Route path={ROUTES.APPOINTMENTS}>
-            {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
-          </Route>
-        </Switch>
-      </main>
-    </>
-  );
+    const setNewAppointments = (newAppointments) => {
+        setAppointments([...appointments, newAppointments]);
+    }
+
+
+    return (
+        <>
+            <nav>
+                <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+                    Contacts
+                </NavLink>
+                <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+                    Appointments
+                </NavLink>
+            </nav>
+            <main>
+                <Switch>
+                    <Route exact path="/">
+                        <Redirect to={ROUTES.CONTACTS}/>
+                    </Route>
+                    <Route path={ROUTES.CONTACTS}>
+                        {/* Add props to ContactsPage */}
+                        <ContactsPage currentContactList={contacts} setNewContacts={setNewContacts}/>
+                    </Route>
+                    <Route path={ROUTES.APPOINTMENTS}>
+                        {/* Add props to AppointmentsPage */}
+                        <AppointmentsPage currentAppointmentList={appointments} setNewAppointments={setNewAppointments} />
+                    </Route>
+                </Switch>
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
